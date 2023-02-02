4ebdcb83f1afa6903d6ec59c173ef1abe53f9a76
Weijian Yang <weijian.yang@rea-group.com>
2023-02-02 18:37:12 +0800

    support eks.

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
 import './src/ecr';
 // import './src/iam';
 import * as aws from '@pulumi/aws';
-import { Output } from '@pulumi/pulumi';
-import * as R from 'ramda';
 import { eksCluster } from './src/eks';
 import { createSubnets } from './src/subnet';
 import { assignOwner, concatName } from './src/utils';
@@ -10,8 +8,10 @@ import { assignOwner, concatName } from './src/utils';
 let vpc = new aws.ec2.Vpc('vpc', {
   cidrBlock: '10.0.0.0/16',
   tags: assignOwner({ Name: concatName('vpc') }),
+  enableDnsHostnames: true,
+  enableDnsSupport: true,
 });
 
 let subnets = createSubnets(vpc, ['ap-east-1a', 'ap-east-1b'], '10.0.0.0/16', 24);
 
-eksCluster(subnets.priSubnets.map(R.prop<Output<string>>('id')));
+eksCluster(subnets.priSubnets);
--- src/eks/index.ts
+++ src/eks/index.ts
@@ -1,5 +1,5 @@
 import * as aws from '@pulumi/aws';
-import { Output } from '@pulumi/pulumi';
+import * as R from 'ramda';
 import { assignOwner, concatName } from '../utils';
 
 let eksClusterRole = new aws.iam.Role('eks-cluster-role', {
@@ -20,14 +20,64 @@ new aws.iam.RolePolicyAttachment('eks-cluster-role-attachment', {
   policyArn: 'arn:aws:iam::aws:policy/AmazonEKSClusterPolicy',
 });
 
-export const eksCluster = (subnetIds: Output<string>[]) => {
-  new aws.eks.Cluster('eks-cluster', {
+let eksNodeGroupRole = new aws.iam.Role('eks-node-group-role', {
+  name: concatName('for-eks-node-group'),
+  assumeRolePolicy: {
+    Version: '2012-10-17',
+    Statement: [{
+      Effect: 'Allow',
+      Action: 'sts:AssumeRole',
+      Principal: { Service: 'ec2.amazonaws.com' },
+    }],
+  },
+  tags: assignOwner({}),
+});
+
+R.forEach(policyName => new aws.iam.RolePolicyAttachment(`eks-node-group-role-attachment-${policyName}`, {
+  role: eksNodeGroupRole.name,
+  policyArn: `arn:aws:iam::aws:policy/${policyName}`,
+}))([
+  'AmazonEC2ContainerRegistryReadOnly',
+  'AmazonEKSWorkerNodePolicy',
+  'AmazonEKS_CNI_Policy',
+]);
+
+export const eksCluster = (subnets: aws.ec2.Subnet[]) => {
+  let subnetIds = subnets.map(R.prop<string>('id'));
+
+  let cluster = new aws.eks.Cluster('eks-cluster', {
     name: concatName('eks'),
     roleArn: eksClusterRole.arn,
     tags: assignOwner({}),
     vpcConfig: {
       subnetIds,
       publicAccessCidrs: ['202.66.38.130/32'],
+      endpointPrivateAccess: true,
+      endpointPublicAccess: true,
     },
+    version: '1.24',
+  }, { dependsOn: [eksClusterRole] });
+
+  new aws.eks.Addon('vpc-cni', {
+    clusterName: cluster.name,
+    addonName: 'vpc-cni',
+    addonVersion: 'v1.12.1-eksbuild.2',
+    resolveConflicts: 'OVERWRITE',
+  });
+
+  new aws.eks.NodeGroup('common-node-group', {
+    nodeGroupName: 'common',
+    clusterName: cluster.name,
+    subnetIds: subnetIds,
+    nodeRoleArn: eksNodeGroupRole.arn,
+    scalingConfig: {
+      desiredSize: 1,
+      maxSize: 2,
+      minSize: 1,
+    },
+    updateConfig: {
+      maxUnavailable: 1,
+    },
+    tags: assignOwner({}),
   });
 };
